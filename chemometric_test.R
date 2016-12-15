ad31460ed185d6c0aa77a2e00a62dd8258603d60
TigerFugu <dlambert.ch@gmail.com>
2016-12-15 14:39:38 +0100

    Et voila et essai=4

---
+++ chemometric_test.R
@@ -0,0 +1,11 @@
+#Diff?rents tests bas? sur le livre Chemometric with R
+library(ChemometricsWithR)
+library(stringr)
+library(knitr)
+data(wines, package="kohonen")
+#grepRaw("_",dir()[1],all=T)
+str_sub(dir()[250],1,grepRaw("_",dir()[250],all=T)[2]-1)
+essai=2+2
+essai
+#Et voilà!!
+  
